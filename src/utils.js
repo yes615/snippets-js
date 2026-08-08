1b3612daa2438332bcef27bed04488454e11ef46
yes615 <77709723+yes615@users.noreply.github.com>
2026-08-08 20:40:01 +0800

    feat: add reverse

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,1 @@
 exports.reverse = (s) => String(s).split('').reverse().join('');
-
-exports.unique = (a) => [...new Set(a)];
-
-exports.clamp = (v, lo, hi) => Math.max(lo, Math.min(v, hi));
-
-exports.chunk = (a, n) => Array.from({ length: Math.ceil(a.length / n) }, (_, i) => a.slice(i * n, i * n + n));
-
-exports.sum = (a) => a.reduce((x, y) => x + y, 0);
